5a7b5914d0ee07c68cf5d04f67f53fa29a3ddedb
AHOUNOU Amos Landry <62993041+Mo1000@users.noreply.github.com>
2023-02-13 09:38:05 +0100

    config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import {BrowserRouter ,Routes ,Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Knowledges from "./pages/Knowledges";
 import Portfolio from "./pages/Portfolio";
 import NotFound from "./pages/NotFound";
 
-
 const App = () => {
-    return (
+  return (
     <>
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Home/>}></Route>
-                <Route path="/competences" element={<Knowledges/>}/>
-                <Route path="/portofolio" element={<Portfolio/>}></Route>
-                <Route path="/contact" element={<Contact/>}></Route>
-                <Route element={<NotFound/>}></Route>
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/cv-react" element={<Home />}></Route>
+          <Route path="/cv-react/competences" element={<Knowledges />} />
+          <Route path="/cv-react/portofolio" element={<Portfolio />}></Route>
+          <Route path="/cv-react/contact" element={<Contact />}></Route>
+          <Route element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-    );
+  );
 };
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,80 +1,98 @@
-import React from 'react';
-import  {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-        <div className="sidebar">
-        <div className="id">
+  return (
+    <div className="sidebar">
+      <div className="id">
         <div className="idContent">
-                <img src="./media/Amos1.jpg" alt="profil-pic"/>
-                <h3>Amos Ahounou</h3>
-            </div>
+          <img src="./media/Amos1.jpg" alt="profil-pic" />
+          <h3>Amos Ahounou</h3>
         </div>
+      </div>
 
-            <div className="navigation">
-                <ul>
-                   <li>
-                       <NavLink exact to="/" activeClassName="navActive">
-                           <i className="fas fa-home"></i>
-                           <span>Acceuil</span>
-                       </NavLink>
-                   </li>
-                    <li>
-                        <NavLink exact to="/competences" activeClassName="navActive">
-                            <i className="fas fa-mountain"></i>
-                            <span>Compétences</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to="/portofolio" activeClassName="navActive">
-                            <i className="fas fa-images"></i>
-                            <span>Portofolio</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to="/contact" activeClassName="navActive">
-                            <i className="fas fa-address-book"></i>
-                            <span>Contact</span>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-            <div className="socialNetwork">
-                <ul>
-                    <li>
-                        <a href ="https://www.linkedin.com/in/amos-ahounou/"
-                        target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </li>
+      <div className="navigation">
+        <ul>
+          <li>
+            <NavLink exact to="/cv-react" activeClassName="navActive">
+              <i className="fas fa-home"></i>
+              <span>Acceuil</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/cv-react/competences"
+              activeClassName="navActive"
+            >
+              <i className="fas fa-mountain"></i>
+              <span>Compétences</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/cv-react/portofolio"
+              activeClassName="navActive"
+            >
+              <i className="fas fa-images"></i>
+              <span>Portofolio</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/cv-react/contact" activeClassName="navActive">
+              <i className="fas fa-address-book"></i>
+              <span>Contact</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="socialNetwork">
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/amos-ahounou/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
 
-                    <li>
-                        <a href ="https://github.com/Mo1000"
-                           target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href ="https://www.twitter.com"
-                           target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href ="https://codepen.io/"
-                           target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-codepen"></i>
-                        </a>
-                    </li>
-                </ul>
-                <div className="signature">
-                    <a>FromAmos-2021</a>
-                </div>
-            </div>
+          <li>
+            <a
+              href="https://github.com/Mo1000"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://codepen.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-codepen"></i>
+            </a>
+          </li>
+        </ul>
+        <div className="signature">
+          <a>FromAmos-2021</a>
         </div>
-
-
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
